909ecd8912c39ec77710ea3d4069ff2d48655c06
Kriselle Tined <kriselle.tined@vopay.com>
2021-07-20 21:03:34 -0700

    test

--- homework/conftest.py
+++ homework/conftest.py
@@ -1,25 +1,9 @@
-from selenium import webdriver
-import pytest
-import allure
-import os
-
-from allure_commons.types import AttachmentType
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome import options
-from selenium.webdriver.chrome.options import Options as chromeOptions
 from selenium.webdriver.firefox.options import Options as firfoxOptions
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver import DesiredCapabilities
 
 from selenium import webdriver
 import pytest
 import allure
 import os
-
-from allure_commons.types import AttachmentType
-from selenium.webdriver import Chrome
-from selenium.webdriver.chrome import options
-from selenium.webdriver.chrome.options import Options
 
 BROWSERS = {
     'firefox': webdriver.Firefox,
@@ -36,15 +20,11 @@ def pytest_runtest_makereport(item, call):
             with open('failures', mode) as f:
                 if 'browser' in item.fixturenames:  # assume this is fixture with webdriver
                     web_driver = item.funcargs['browser']
-                # else:
-                #     print('Fail to take screen-shot')
-                #     return
             allure.attach(
                 web_driver.get_screenshot_as_png(),
                 name='screenshot',
                 attachment_type=allure.attachment_type.PNG
             )
-            # allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
         except Exception as e:
             print('Fail to take screen-shot: {}'.format(e))
 
@@ -54,7 +34,6 @@ def browser(request):
     global driver
     if request.param == 'firefox':
         options = firfoxOptions()
-        # options.add_argument('log-level=3')
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--disable-extensions")
         options.add_argument("--proxy-server='direct://'")
